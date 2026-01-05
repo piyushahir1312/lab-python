8b59380a08f51164023fc9185cc6dc8b28ce4548
piyushahir1312 <piyushahir1312@gmail.com>
2026-01-05 10:28:16 +0530

    Add files via upload

---
+++ d2python.py
@@ -0,0 +1,38 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> x=15
+>>> print (x)
+15
+>>> y=9
+>>> print (-y)
+-9
+>>> x=56
+>>> y=99
+>>> print (x+y)
+155
+>>> x=(77)
+>>> y=(39)
+>>> print(x-y)
+38
+>>> x=69
+>>> y=53
+>>> print(x*y)
+3657
+>>> x=99
+>>> y=2
+>>> print (x/y)
+49.5
+>>> x=True
+>>> y=False
+>>> print(x)
+True
+>>> print(Y)
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    print(Y)
+NameError: name 'Y' is not defined
+>>> print(y)
+False
+>>> 
