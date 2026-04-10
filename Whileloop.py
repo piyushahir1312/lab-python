298c9be4abb8794f17fd9a20d32646cd68e0285c
piyushahir1312 <piyushahir1312@gmail.com>
2026-04-10 09:10:17 +0530

    Implement multiple while loops for various tasks

---
+++ Whileloop.py
@@ -0,0 +1,61 @@
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+
+    
+1
+2
+3
+4
+5
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+
+    
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+n=int(input("Enter n:"))
+Enter n:5
+i=1
+s=0
+while i<=n:
+    s=s+i
+    i=i+1
+    print("sum=",s)
+
+    
+sum= 1
+sum= 3
+sum= 6
+sum= 10
+sum= 15
+num=int(input("Enter Number:"))
+Enter Number:15
+i=1
+while i<=10:
+    print(num,"x",i,"=",num*i)
+    i=i+1
+
+    
+15 x 1 = 15
+15 x 2 = 30
+15 x 3 = 45
+15 x 4 = 60
+15 x 5 = 75
+15 x 6 = 90
+15 x 7 = 105
+15 x 8 = 120
+15 x 9 = 135
+15 x 10 = 150
