7fe98575403dedc6dff8bc7179713ed375b86373
piyushahir1312 <piyushahir1312@gmail.com>
2026-04-10 09:12:03 +0530

    Add various range examples and print statements

---
+++ Range.py
@@ -0,0 +1,91 @@
+for i in range(1,6):
+    print(i)
+
+1
+2
+3
+4
+5
+for a in range (3):
+    print("hello")
+
+    
+hello
+hello
+hello
+for b in range(1,11):
+    print(b)
+
+    
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+for i in range (1,21):
+    if i%2==0:
+        print(i)
+
+        
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+for i in range (1,16):
+    if i%2==0:
+        print(i)
+
+        
+2
+4
+6
+8
+10
+12
+14
+for i in range (1,11):
+    print("5x",i,"=",5*i)
+
+    
+5x 1 = 5
+5x 2 = 10
+5x 3 = 15
+5x 4 = 20
+5x 5 = 25
+5x 6 = 30
+5x 7 = 35
+5x 8 = 40
+5x 9 = 45
+5x 10 = 50
+name="atmiya"
+for letter in name:
+    print(letter)
+
+    
+a
+t
+m
+i
+y
+a
+    
+numbers = [10,20,30,40]
+for n in numbers:
+    print(n)
+
+    
+10
+20
+30
+40
