8586b5f7f893802ef5ec972dbb2341f79a7f1409
piyushahir1312 <piyushahir1312@gmail.com>
2026-04-10 09:11:01 +0530

    Add basic input and output functionality in Io.py

---
+++ Io.py
@@ -0,0 +1,22 @@
+a=("Hello")
+print(a)
+b=("Atmiya")
+print(b)
+a=input(("Enter your name : "))
+print("Hello",a)
+b=input(("Enter Number :"))
+print("Number is :",b)
+x=int(input("Enter value :"))
+print(x)
+y=int(input("Enter numbers :"))
+print(y)
+p=float(input("Enter percentage :"))
+print(p)
+q=float(input("Enter measure :"))
+print(q)
+a=True
+b=False
+print(a<b)
+x=9
+y=5
+print(a>b)
